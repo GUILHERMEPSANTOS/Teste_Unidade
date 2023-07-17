850586df441102494db4a715c6261a9a0599c3cf
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-07-17 19:55:17 -0300

    test: teste que assegura resultado de soma

---
+++ 1.1_Testes_Basicos/Demo/Calculadora.cs
@@ -0,0 +1,14 @@
+﻿namespace Demo;
+
+public class Calculadora
+{
+    public double Somar(double primeiroNumero, double segundoNumero)
+    {
+        return primeiroNumero + segundoNumero;
+    }
+
+    public int Dividir(int primeiroNumero, int segundoNumero)
+    {
+        return primeiroNumero / segundoNumero;
+    }
+}
