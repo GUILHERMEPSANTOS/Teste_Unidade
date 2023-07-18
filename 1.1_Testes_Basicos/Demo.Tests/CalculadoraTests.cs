0d3d2ab82e39d156f0ca9328f00f383153b52d6b
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-07-17 21:20:50 -0300

    test: teste com theory

--- 1.1_Testes_Basicos/Demo.Tests/CalculadoraTests.cs
+++ 1.1_Testes_Basicos/Demo.Tests/CalculadoraTests.cs
@@ -16,10 +16,10 @@ public class CalculadoraTests
     }
 
     [Theory]
-    [InlineData(2,2,4)]
-    [InlineData(2,3,5)]
-    [InlineData(2,4,6)]
-    [InlineData(2,5,7)]
+    [InlineData(2, 2, 4)]
+    [InlineData(2, 3, 5)]
+    [InlineData(2, 4, 6)]
+    [InlineData(2, 5, 7)]
     public void Calculadora_Somar_RetornarValorSomaCorretos(double primeiroNumero, double segundoNumero, double total)
     {
         //Arrange
@@ -30,5 +30,18 @@ public class CalculadoraTests
 
         //Assert
         Assert.Equal(total, resultado);
+    }
+
+    [Fact]
+    public void Calculadora_Somar_NaoDeveSerIgual()
+    {
+        //Arrange
+        var calculadora = new Calculadora();
+
+        //Act
+        var resultado = calculadora.Somar(1.9, 1.299999999999998);
+
+        //Assert
+        Assert.NotEqual(3.3, resultado);
     }
 }
