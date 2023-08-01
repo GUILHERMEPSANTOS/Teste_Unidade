76a651ca8953dc2a55fdf1ca519d5845df674ca3
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2023-07-31 21:01:36 -0300

    test: ensure the client is valid depending on the scenario with Bogus

---
+++ 2.2_Features/Features.Tests/Dados Humanos/ClienteBogusTests.cs
@@ -0,0 +1,45 @@
+﻿using Features.Tests.Fixture;
+
+namespace Features.Tests.Dados_Humanos
+{
+    [Collection(nameof(ClienteBogusCollection))]
+    public class ClienteBogusTests
+    {
+        private readonly ClienteTestsBogusFixture _clienteTestsFixture;
+
+        public ClienteBogusTests(ClienteTestsBogusFixture clienteTestsFixture)
+        {
+            _clienteTestsFixture = clienteTestsFixture;
+        }
+
+        [Fact(DisplayName = "Novo Cliente Válido")]
+        [Trait("Categoria", "Cliente Bogus Testes")]
+        public void Cliente_NovoCliente_DeveSerValido()
+        {
+            //Arrange
+            var cliente = _clienteTestsFixture.GerarClienteValido();
+
+            //Act
+            var isValid = cliente.EhValido();
+
+            //Assert
+            Assert.True(isValid);
+            Assert.Empty(cliente.ValidationResult.Errors);
+        }
+
+        [Fact(DisplayName = "Novo Cliente Inválido")]
+        [Trait("Categoria", "Cliente Bogus Testes")]
+        public void Cliente_NovoCliente_DeveSerInvalido()
+        {
+            //Arrange
+            var cliente = _clienteTestsFixture.GerarClienteInvlaido();
+
+            //Act
+            var isValid = cliente.EhValido();
+
+            //Assert
+            Assert.False(isValid);
+            Assert.NotEmpty(cliente.ValidationResult.Errors);
+        }
+    }
+}
